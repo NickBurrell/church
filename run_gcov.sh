831aadbf8ea422fee7641386779efda0e8d3dd28
Nicholas Burrell <zero_frost@protonmail.com>
2018-03-24 17:52:21 -0700

    Updated CMake build for tests

---
+++ run_gcov.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for filename in `find . | egrep '\.cpp'`
+do
+    gcov -n -o . $filename > /dev/null
+done
---
+++ tests/test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+#include "gtest/gtest.h"
+
+TEST(test1, Test) {
+    EXPECT_EQ(1, 2);
+}
+
+int main(int argc, char** argv) {
+    std::cout << "test" << std::endl;
+    RUN_ALL_TESTS();
+}
